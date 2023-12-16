b3a157dd40f32c2d08c0d6dd1db4228d4cd4249a
pranav-k-jha <jha_k.pranav@live.com>
2023-12-16 17:05:30 -0500

    modified anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,50 @@
 */
 
 function isAnagram(str1, str2) {
+  let counterFreq1 = {};
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  for (let i = 0; i < str1.length; i++) {
+    const char = str1[i];
+
+    counterFreq1[char] = (counterFreq1[char] || 0) + 1;
+  }
+  console.log(counterFreq1);
+
+  let counterFreq2 = {};
+  for (let i = 0; i < str2.length; i++) {
+    const char = str2[i];
+
+    counterFreq2[char] = (counterFreq2[char] || 0) + 1;
+  }
+  console.log(counterFreq2);
+
+  const keys1 = Object.keys(counterFreq1);
+  console.log(keys1);
+  const keys2 = Object.keys(counterFreq2);
+  console.log(keys2);
+
+  if (keys1.length != keys2.length) {
+    console.log("Not an Anagram");
+    return false;
+  }
+
+  for (let i = 0; i < keys1.length; i++) {
+    const char = keys1[i];
+    if (counterFreq1[char] !== counterFreq2[char]) {
+      console.log("Not an Anagram!");
+      return false;
+    }
+  }
+
+  console.log("Anagram");
+  return true;
 }
+
+str1 = "abcbc";
+str2 = "bcabac";
+
+isAnagram(str1, str2);
 
 module.exports = isAnagram;
