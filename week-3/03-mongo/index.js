6d9db0c8b2a5889bd0f4d1980a95f852b7b62113
pranav-k-jha <jha_k.pranav@live.com>
2023-12-27 20:35:22 -0500

    removed mongodb url

--- week-3/03-mongo/index.js
+++ week-3/03-mongo/index.js
@@ -1,5 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+// const adminRouter = require("./routes/admin");
+// const userRouter = require("./routes/user");
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://pranavjha:v52T8EV9J6prw5bF@cluster0.tciw1xy.mongodb.net/UsersApp"
-);
 
+
+
+/* Admin Routes:
+POST /admin/signup Description: 
+Creates a new admin account. Input Body: { username: 'admin', password: 'pass' } Output: { message: 'Admin created successfully' }
+
+*/
+// Middleware for parsing request bodies
+app.use(bodyParser.json());
+// app.use("/admin", adminRouter);
+// app.use("/user", userRouter);
+
+const PORT = 3000;
+
+app.post("/admin/signup", (req, res) => {
+  const adminSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+  });
+
+  const adminModel = mongoose.model("Admin", adminSchema);
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user1 = new adminModel({ username, password });
+
+  user1.save();
+
+  // Save the new admin to the database
+  res.json({ message: "Admin created successfully" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
