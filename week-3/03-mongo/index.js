0150f95b809a548fbbc4c3ba8dd7cb538cc2561e
pranav-k-jha <jha_k.pranav@live.com>
2023-12-28 14:00:48 -0500

    completed all todos

--- week-3/03-mongo/index.js
+++ week-3/03-mongo/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -33,6 +34,8 @@ const courseSchema = new mongoose.Schema({
   imageLink: String,
 });
 const Course = mongoose.model("Course", courseSchema);
+
+const PurchasedCourse = mongoose.model("PurchasedCourse", courseSchema);
 
 app.post("/admin/signup", (req, res) => {
   const { username, password } = req.body;
@@ -118,13 +121,45 @@ app.post("/users/courses/:courseId", async (req, res) => {
     if (!user) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
+    const course = await PurchasedCourse.findOne({ _id: courseId });
+    if (course) {
+      return res.status(409).json({ message: "Course already purchased" });
+    }
+    const courseToPurchase = await Course.findOne({ _id: courseId });
+    if (!courseToPurchase) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    const purchasedCourse = new PurchasedCourse({
+      ...courseToPurchase.toObject(),
+      purchased: true,
+    });
+
+    await purchasedCourse.save();
     res.status(201).json({
       message: "Course purchased successfully",
-      courseId,
+      course: purchasedCourse,
     });
   } catch (err) {
-    console.error("Error creating course: ", err);
-    res.status(500).json({ message: "Error creating course" });
+    console.error("Error purchasing course: ", err);
+    res.status(500).json({ message: "Error purchasing course" });
+  }
+});
+/* 
+POST /users/courses/:courseId Description: Purchases a course. courseId in the URL path should be replaced with the ID of the course to be purchased. Input: Headers: { 'username': 'username', 'password': 'password' } Output: { message: 'Course purchased successfully' }
+
+GET /users/purchasedCourses Description: Lists all the courses purchased by the user. Input: Headers: { 'username': 'username', 'password': 'password' } Output: { purchasedCourses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] } */
+
+app.get("/users/purchasedCourses", async (req, res) => {
+  const { username, password } = req.headers;
+  try {
+    const user = await User.findOne({ username, password });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const purchasedCourses = await PurchasedCourse.find();
+    res.send(purchasedCourses);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching purchased courses" });
   }
 });
 
