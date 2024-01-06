9ad5865a2ca31253ef8c15520970d128a9dce672
pranav-k-jha <jha_k.pranav@live.com>
2024-01-06 13:30:21 -0500

    react basics

--- week-4/react-basics/index.js
+++ week-4/react-basics/index.js
@@ -1,32 +1,45 @@
-let count = 0;
-function addToDo() {
-  title = document.getElementById("title").value;
-  description = document.getElementById("description").value;
-  const p = document.getElementById("added");
-  p.innerHTML = "Added ToDo Items";
-  
-  const ul = document.createElement("ul");
-  const li = document.createElement("li");
-  const button = document.createElement("button");
-  button.innerHTML = "Mark as Done";
-  button.setAttribute("id", `button ${count}`);
-  ul.setAttribute("id", `ul ${count}`);
-  button.setAttribute("onclick", `markDone(${count})`);
-  console.log(button);
-  ul.innerHTML = title;
-
-  li.innerHTML = description;
-
-  document.getElementById("addedItems").appendChild(ul).appendChild(li);
-  document.getElementById("addedItems").appendChild(button);
-
-  console.log(document.getElementById("addedItems"));
-  count++;
-}
+let countId = 1;
 
 function markDone(id) {
   const button = document.getElementById(`button ${id}`);
   button.innerHTML = "Done";
-  ul = document.getElementById(`ul ${id}`);
+  ul = document.getElementById(id);
   ul.setAttribute("class", "done");
+}
+
+function createChild(title, description, countId) {
+  
+  const para = document.getElementById("added");
+  para.innerHTML = "Added ToDo Items";
+
+  const childUl = document.createElement("ul");
+  childUl.setAttribute("id", `${countId}`);
+  childUl.innerHTML = title;
+
+  const childLi = document.createElement("li");
+  childLi.innerHTML = description;
+
+  const button = document.createElement("button");
+  button.innerHTML = "Mark as Done";
+  button.setAttribute("id", `button ${countId}`);
+  button.setAttribute("onclick", `markDone(${countId})`);
+
+  childUl.appendChild(childLi);
+  childUl.appendChild(button);
+
+  console.log(button);
+
+  return childUl;
+
+  // document.getElementById("addedItems").appendChild(ul).appendChild(li);
+  // document.getElementById("addedItems").appendChild(button);
+}
+
+function addToDo() {
+  title = document.getElementById("title").value;
+  description = document.getElementById("description").value;
+  const parent = document.getElementById("addedItems");
+
+  parent.appendChild(createChild(title, description, countId++));
+  console.log(parent);
 }
