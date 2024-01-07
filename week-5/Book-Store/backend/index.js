4d4c7afb65933e1e3a563b400b95b3d2c4e5bc93
pranav-k-jha <jha_k.pranav@live.com>
2024-01-07 01:06:52 -0500

    added get all and post routes

--- week-5/Book-Store/backend/index.js
+++ week-5/Book-Store/backend/index.js
@@ -1,15 +1,60 @@
-import dotenv from "dotenv";
-dotenv.config({ path: '../.env'});
+import connectDB, { app } from "./config.js";
+import { Book } from "./models/bookModel.js";
+import express from "express";
 
-import connectDB, {app} from "./config.js";
-
-
-
-
+app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.status(234).send("Welcome to MERN Stack");
+});
+
+//ROute for save a new Book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(300).send({ message: error.message });
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const id = req.params.id;
+    console.log(id);
+
+    const book = await Book.findById(id);
+
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(300).send({ message: error.message });
+  }
 });
 
 connectDB();
-
-
